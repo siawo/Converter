b420bdb163c2bf3054960340fd4a0ecca622ce3b
siawo <owaisitsme1@gmail.com>
2017-08-13 10:58:00 +0530

    cardinal converter done

---
+++ src/js/Cardinal.js
@@ -0,0 +1,12 @@
+import {Num2Words} from './Num2Words.js';
+export class Cardinal {
+  constructor (input) {
+    this.input = input;
+  }
+  output () {
+    let num = new Num2Words(this.input.word);
+    this.input.word = num.find();
+    console.log(this.input.word);
+    return this.input;
+  }
+}
