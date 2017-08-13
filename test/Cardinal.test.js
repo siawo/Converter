d254955ee42acd5bbf3c0209557760b8f036bfc2
siawo <owaisitsme1@gmail.com>
2017-08-13 22:34:12 +0530

    added new test and changed the text

--- test/Cardinal.test.js
+++ test/Cardinal.test.js
@@ -1,25 +1,25 @@
 import {Cardinal} from '../src/js/Cardinal.js';
-test('adds 1 + 2 to equal 3', () => {
+test('5 digit input', () => {
   let card = new Cardinal('12345');
   let out = card.output();
   expect(out).toBe('twelve thousand three hundred and forty five');
 });
-test('adds 1 + 2 to equal 3', () => {
+test('check for zero', () => {
   let card = new Cardinal('0');
   let out = card.output();
   expect(out).toBe('zero');
 });
-test('adds 1 + 2 to equal 3', () => {
+test('xheck for out of limit', () => {
   let card = new Cardinal('1234567890123456789012345678901234567890123456789');
   let out = card.output();
   expect(out).toBe('Out of Limit');
 });
-test('adds 1 + 2 to equal 3', () => {
+test('check for a three digit number', () => {
   let card = new Cardinal('116');
   let out = card.output();
   expect(out).toBe('one hundred and sixteen');
 });
-test('adds 1 + 2 to equal 3', () => {
+test('check for millions', () => {
   let card = new Cardinal('10000021');
   let out = card.output();
   expect(out).toBe('ten million twenty one');
--- test/SuffixPrefix.test.js
+++ test/SuffixPrefix.test.js
@@ -1,10 +1,10 @@
 import {SuffixPrefix} from '../src/js/Suffix_prefix.js';
-test('adds 1 + 2 to equal 3', () => {
+test('check for suffix', () => {
   let suf = new SuffixPrefix('@ab12');
   let out = suf.output();
   expect(out).toBe('@abtwelve');
 });
-test('adds 1 + 2 to equal 3', () => {
+test('check for prefix', () => {
   let pref = new SuffixPrefix('3rd');
   let out = pref.output();
   expect(out).toBe('third');
