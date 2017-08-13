a575d949345008a61b6f45498015dad3e1da0a32
siawo <owaisitsme1@gmail.com>
2017-08-13 10:35:38 +0530

    initial setup of webpack, eslint, yarn done

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: './src/js/Identifier.js',
+  output: {
+    filename: './build/bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env']
+        }
+      }
+    ]
+  }
+};
