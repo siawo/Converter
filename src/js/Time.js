dc269eec8d2412a891a8728ad5875f84545087d5
siawo <owaisitsme1@gmail.com>
2017-08-13 10:57:22 +0530

    time converter done

---
+++ src/js/Time.js
@@ -0,0 +1,17 @@
+import {Num2Words} from './Num2Words.js';
+export class Time {
+  constructor (input) {
+    this.input = input;
+    this.out = [];
+  }
+  output () {
+    let inp = this.input.word.split(/[:]/);
+    for (let i = 0; i < inp.length; i++) {
+      let num = new Num2Words(inp[i]);
+      this.out.push(num.find());
+    }
+    var word = '';
+    word = this.out[0] + ':' + this.out[1];
+    console.log(word);
+  }
+}
