014a00cc4eafa45ffad043f5f0168287cddd7c71
siawo <owaisitsme1@gmail.com>
2017-08-13 10:54:19 +0530

    phone no converter done

---
+++ src/js/Phone.js
@@ -0,0 +1,18 @@
+import {Num2Words} from './Num2Words.js';
+export class Phone {
+  constructor (input) {
+    this.input = input;
+  }
+  output () {
+    let word = '';
+    for (let i = 0; i < this.input.word.length; i++) {
+      if (!isNaN(parseInt(this.input.word[i]))) {
+        let num = new Num2Words(this.input.word[i]);
+        word += num.find() + ' ';
+      } else {
+        word += this.input.word[i] + ' ';
+      }
+    }
+    console.log(word.trim());
+  }
+}
