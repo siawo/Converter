f2417c5f71c572f82df2cf7c7497a24d69c16e27
siawo <owaisitsme1@gmail.com>
2017-08-13 10:53:03 +0530

    decider done

---
+++ src/js/Decider.js
@@ -0,0 +1,30 @@
+import {Cardinal} from './Cardinal.js';
+import {SpecialMiddle} from './Special_middle.js';
+import {SuffixPrefix} from './Suffix_prefix.js';
+export class Decider {
+  constructor (out) {
+    this.input = out;
+    // console.log(this.input);
+  }
+  decide () {
+    let dig = /^\d+$/;
+    let mid = /^\d{1,2}[/-]\d{1,2}[/-]\d{2,4}$|^\d{1,2}[:]\d{2}$|^\d+[/.]\d+$|^0\d{10}$|^[+]\d{2}[-| ]\d{10}/;
+    // let sufpref = /^[A-Za-z]*\d+[A-Za-z]*/;
+    let suf = /^[@#`~$%^&*()_\-+={}\\|:;"'?.>,<A-Za-z]*\d+$/;
+    let pref = /^\d+[@#`~$%^&*()_\-+={}\\|:;"'?.>,<A-Za-z]*/;
+    // console.log(mid.test(this.input[i].word));
+    if (dig.test(this.input.word)) {
+      let cardinalOb = new Cardinal(this.input);
+      let out = cardinalOb.output();
+      return out;
+    } else if (mid.test(this.input.word)) {
+      // console.log(i);
+      let special = new SpecialMiddle(this.input);
+      special.whichSpecialMiddle();
+    } else if (suf.test(this.input.word) || pref.test(this.input.word)) {
+      let sufprefOb = new SuffixPrefix(this.input);
+      sufprefOb.output();
+      // console.log(this.input);
+    }
+  }
+}
